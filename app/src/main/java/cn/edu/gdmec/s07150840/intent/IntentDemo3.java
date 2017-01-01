d7f2f1bda8368da8e5afa9b5cdc8ac23aab6b1e4
gdmec07150840 <www.xiaotao2009@qq.com>
2017-01-01 18:48:28 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150840/intent/IntentDemo3.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.s07150840.intent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hasee on 2016/12/27.
+ */
+public class IntentDemo3 extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
